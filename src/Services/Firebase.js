7402ac6693860be8fd932b105ce23502b46cc3cf
Cole Goodling <cole@helpfulhuman.com>
2017-11-05 19:32:07 -0800

    removed config vars from source code

--- src/Services/Firebase.js
+++ src/Services/Firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDc5H9Tlo_GbWJw7O-MHnjpb0zDy53SWuQ",
-  authDomain: "blackjackapp-34992.firebaseapp.com",
-  databaseURL: "https://blackjackapp-34992.firebaseio.com",
-  projectId: "blackjackapp-34992",
-  storageBucket: "blackjackapp-34992.appspot.com",
-  messagingSenderId: "683020977962"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: `${process.env.REACT_APP_FIREBASE_PROJECTID}.firebaseapp.com`,
+    databaseURL: `https://${process.env.REACT_APP_FIREBASE_PROJECTID}.firebaseio.com`,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: `${process.env.REACT_APP_FIREBASE_PROJECTID}.appspot.com`,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID
 };
 
 firebase.initializeApp(config);
